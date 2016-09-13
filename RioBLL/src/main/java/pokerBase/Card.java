dcee5bae51f3cab8d31ffa9df30e25b6a93cab2b
Daniel <Daniel@128.4.101.147>
2016-09-13 11:54:18 -0400

    test

--- RioBLL/src/main/java/pokerBase/Card.java
+++ RioBLL/src/main/java/pokerBase/Card.java
@@ -4,7 +4,27 @@ import java.util.Comparator;
 
 public class Card {
 
+	private int iCardNbr;
+	private int iCardSuit;
+	private int iCardRank; 
 	
+	// should these be private methods?
+	public Card(int iCardNbr, int iCardSuit, int iCardRank) {
+		this.iCardNbr = iCardNbr;
+		this.iCardSuit = iCardSuit;
+		this.iCardRank = iCardSuit;
+	}
 	
+	public int getiCardNbr() {
+		return iCardNbr;
+	}
+	
+	public int getiCardSuit() {
+		return iCardSuit;
+	}
+	
+	public int getiCardRank() {
+		return iCardRank;
+	}
 	
 }
